bf6e49a8fb03bd364459a949b2b3eb4c6e51f1be
gedeyandi456 <gede.yandi@blockchainzoo.com>
2019-01-29 18:38:33 +0800

    add initial colors, styles, and strings resources

---
+++ lib/src/res/values/strings.dart
@@ -0,0 +1,8 @@
+class Strings {
+  static String appTitle = 'V-Play';
+  static String drawerTitle = 'V-Play';
+  static String drawerHomeTitle = 'Home';
+  static String homeTag = 'home_page';
+  static String homeAppBarTitle = 'Home Page';
+  static String homeBodyTitle = 'This is home page';
+}
