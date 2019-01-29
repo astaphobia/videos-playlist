ba3bbeb8ea736eee4e34a118f6067634f920a6c1
gedeyandi456 <gede.yandi@blockchainzoo.com>
2019-01-29 18:40:52 +0800

    implement initial resource strings to navbar.dart

--- lib/src/components/navbar.dart
+++ lib/src/components/navbar.dart
@@ -1,22 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:vplay/src/res/values/strings.dart';
 
 class NavBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Drawer(
       child: ListView(
         padding: EdgeInsets.zero,
         children: <Widget>[
           DrawerHeader(
             child: Center(
-              child: Text("VPLAY"),
+              child: Text(Strings.drawerTitle),
             ),
           ),
           ListTile(
-            title: Text("Home"),
-            onTap: () => Navigator.of(context).pushNamed("home_page"),
+            title: Text(Strings.drawerHomeTitle),
+            onTap: () => Navigator.of(context).pushNamed(Strings.homeTag),
           )
         ],
       ),
